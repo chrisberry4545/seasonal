4b05f30dd63dd41d5e1aa2bba8345a89239b19b6
chrisberry4545 <chrisberry4545@googlemail.com>
2020-01-14 18:01:25 +0000

    Updates

---
+++ precache-manifest.c123beec1def12f5a66f2a5ca6c91a6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "4c6c322b5141ce963636",
+    "url": "/static/js/main.4c6c322b.chunk.js"
+  },
+  {
+    "revision": "d60f095a3fa2952e1b94",
+    "url": "/static/js/2.d60f095a.chunk.js"
+  },
+  {
+    "revision": "4c6c322b5141ce963636",
+    "url": "/static/css/main.7be68c1e.chunk.css"
+  },
+  {
+    "revision": "a3637e46c7bb188a75c017e22eb31142",
+    "url": "/index.html"
+  }
+];
