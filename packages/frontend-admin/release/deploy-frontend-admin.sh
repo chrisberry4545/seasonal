34a2bf911e52d39f9d884ab20c5e493a0817017a
chrisberry4545 <chrisberry4545@googlemail.com>
2020-02-08 19:58:18 +0000

    set git config in script

--- packages/frontend-admin/release/deploy-frontend-admin.sh
+++ packages/frontend-admin/release/deploy-frontend-admin.sh
@@ -1,4 +1,6 @@
 git init
+git config user.email "chrisbery4545@googlemail.com"
+git config user.name "chrisbery4545"
 git add .
 git remote add origin git@github.com:chrisberry4545/seasonal-admin.git
 yarn build
