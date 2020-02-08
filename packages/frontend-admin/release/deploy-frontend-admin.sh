f79890fdccea700786e81f206d5b8b1c0a98617b
chrisberry4545 <chrisberry4545@googlemail.com>
2020-02-08 21:33:47 +0000

    fixed links

--- packages/frontend-admin/release/deploy-frontend-admin.sh
+++ packages/frontend-admin/release/deploy-frontend-admin.sh
@@ -3,6 +3,6 @@ git config user.email "chrisbery4545@googlemail.com"
 git config user.name "chrisbery4545"
 git add .
 git remote add origin git@github.com:chrisberry4545/seasonal-admin.git
-yarn build
+PUBLIC_URL=./ yarn build
 yarn gh-pages
 rm -rf .git/
