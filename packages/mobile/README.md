1c98acab060ab80978e18e18f1670ece87a55828
chrisberry4545 <chrisberry4545@googlemail.com>
2020-02-21 07:30:59 +0000

    fixed ios store link

--- packages/mobile/README.md
+++ packages/mobile/README.md
@@ -2,4 +2,9 @@
 
 Mobile app for Eat Seasonal built using React Native.
 
+Android App:
+
 https://play.google.com/store/apps/details?id=com.chrisbdev.seasonal
+
+iOS App:
+https://apps.apple.com/us/app/eat-seasonal/id1496551124?ls=1
--- packages/mobile/src/config/index.ts
+++ packages/mobile/src/config/index.ts
@@ -3,5 +3,5 @@ import { Platform } from 'react-native';
 export const PRIVACY_POLICY_URL: string =
   'https://chrisberry4545.github.io/eat-seasonal-privacy-policy/';
 export const STORE_URL: string = Platform.OS === 'ios'
-  ? 'https://play.google.com/store/apps/details?id=com.chrisbdev.seasonal'
+  ? 'https://apps.apple.com/us/app/eat-seasonal/id1496551124?ls=1'
   : 'market://details?id=com.chrisbdev.seasonal';
