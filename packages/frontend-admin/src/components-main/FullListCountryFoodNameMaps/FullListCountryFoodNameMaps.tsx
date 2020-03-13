d4e6e510e308beeb4e8993c96731f37ccef43498
chrisberry4545 <chrisberry4545@googlemail.com>
2020-03-13 07:48:02 +0000

    show country and food/recipe in map lists

--- packages/frontend-admin/src/components-main/FullListCountryFoodNameMaps/FullListCountryFoodNameMaps.tsx
+++ packages/frontend-admin/src/components-main/FullListCountryFoodNameMaps/FullListCountryFoodNameMaps.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { getAllCountryFoodNameMap, deleteCountryToFoodNameMap } from '../../services';
+import { getAllCountryFoodNameMap, deleteCountryToFoodNameMap, getAllFood, getAllCountries } from '../../services';
 import {
   IGetAuthorizedBackendDataProps,
   GetAuthorizedBackendData
@@ -8,7 +8,12 @@ import { ICountryFoodNameMap } from '@chrisb-dev/seasonal-shared';
 import { ROUTES } from '../../config';
 import { FullList } from '../FullList/FullList';
 
-const FullListCountryFoodNameMapInner: FC<IGetAuthorizedBackendDataProps<ICountryFoodNameMap[]>> = ({
+interface ICountryFoodNameMapViewItem extends ICountryFoodNameMap {
+  countryName: string | undefined;
+  foodName: string | undefined;
+}
+
+const FullListCountryFoodNameMapInner: FC<IGetAuthorizedBackendDataProps<ICountryFoodNameMapViewItem[]>> = ({
   items,
   reload
 }) => (
@@ -16,18 +21,42 @@ const FullListCountryFoodNameMapInner: FC<IGetAuthorizedBackendDataProps<ICountr
     title='Country Food Name Map'
     items={items}
     getItemId={(item) => item.id}
-    getItemName={(item) => item.name}
+    getItemName={(item) => `${item.countryName} - ${item.foodName} - ${item.name}`}
     getItemEditUrl={(item) => `${ROUTES.COUNTRY_FOOD_NAME_MAP}/${ROUTES.EDIT}/${item.id}`}
     deleteItemFunc={(item) => deleteCountryToFoodNameMap(item.id).then((country) => {
       if (reload) {
         reload();
       }
-      return country;
+      return country as ICountryFoodNameMapViewItem;
     })}
   />
 );
 
-export const FullListCountryFoodNameMaps = GetAuthorizedBackendData<ICountryFoodNameMap[]>(
+export const FullListCountryFoodNameMaps = GetAuthorizedBackendData<ICountryFoodNameMapViewItem[]>(
   FullListCountryFoodNameMapInner,
-  getAllCountryFoodNameMap
+  async () => {
+    const [
+      foodNameMaps,
+      food,
+      countries
+    ] = await Promise.all([
+      getAllCountryFoodNameMap(),
+      getAllFood(),
+      getAllCountries()
+    ]);
+    return foodNameMaps.map((foodNameMap) => {
+      const matchingFood =
+        food.find((foodItem) => foodNameMap.foodId === foodItem.id);
+      const matchingCountry =
+        countries.find((country) => foodNameMap.countryId === country.id);
+      return {
+        ...foodNameMap,
+        countryName: matchingCountry && matchingCountry.name,
+        foodName: matchingFood && matchingFood.name
+      };
+    }).sort((a, b) => (
+      (a && a.countryName ? a.countryName : '') > (b && b.countryName ? b.countryName : '')
+        ? -1 : 1
+    ));
+  }
 );
--- packages/frontend-admin/src/components-main/FullListCountryRecipeNameMaps/FullListCountryRecipeNameMaps.tsx
+++ packages/frontend-admin/src/components-main/FullListCountryRecipeNameMaps/FullListCountryRecipeNameMaps.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { getAllCountryRecipeNameMap, deleteCountryToRecipeNameMap } from '../../services';
+import { getAllCountryRecipeNameMap, deleteCountryToRecipeNameMap, getAllRecipes, getAllCountries } from '../../services';
 import {
   IGetAuthorizedBackendDataProps,
   GetAuthorizedBackendData
@@ -8,7 +8,12 @@ import { ICountryRecipeNameMap } from '@chrisb-dev/seasonal-shared';
 import { ROUTES } from '../../config';
 import { FullList } from '../FullList/FullList';
 
-const FullListCountryRecipeNameMapInner: FC<IGetAuthorizedBackendDataProps<ICountryRecipeNameMap[]>> = ({
+interface ICountryRecipeNameMapViewItem extends ICountryRecipeNameMap {
+  countryName: string | undefined;
+  recipeName: string | undefined;
+}
+
+const FullListCountryRecipeNameMapInner: FC<IGetAuthorizedBackendDataProps<ICountryRecipeNameMapViewItem[]>> = ({
   items,
   reload
 }) => (
@@ -16,18 +21,42 @@ const FullListCountryRecipeNameMapInner: FC<IGetAuthorizedBackendDataProps<ICoun
     title='Country Recipe Name Map'
     items={items}
     getItemId={(item) => item.id}
-    getItemName={(item) => item.name}
+    getItemName={(item) => `${item.countryName} - ${item.recipeName} - ${item.name}`}
     getItemEditUrl={(item) => `${ROUTES.COUNTRY_RECIPE_NAME_MAP}/${ROUTES.EDIT}/${item.id}`}
     deleteItemFunc={(item) => deleteCountryToRecipeNameMap(item.id).then((country) => {
       if (reload) {
         reload();
       }
-      return country;
+      return country as ICountryRecipeNameMapViewItem;
     })}
   />
 );
 
-export const FullListCountryRecipeNameMaps = GetAuthorizedBackendData<ICountryRecipeNameMap[]>(
+export const FullListCountryRecipeNameMaps = GetAuthorizedBackendData<ICountryRecipeNameMapViewItem[]>(
   FullListCountryRecipeNameMapInner,
-  getAllCountryRecipeNameMap
+  async () => {
+    const [
+      recipeNameMaps,
+      recipes,
+      countries
+    ] = await Promise.all([
+      getAllCountryRecipeNameMap(),
+      getAllRecipes(),
+      getAllCountries()
+    ]);
+    return recipeNameMaps.map((recipeNameMap) => {
+      const matchingRecipe =
+        recipes.find((recipe) => recipeNameMap.recipeId === recipe.id);
+      const matchingCountry =
+        countries.find((country) => recipeNameMap.countryId === country.id);
+      return {
+        ...recipeNameMap,
+        countryName: matchingCountry && matchingCountry.name,
+        recipeName: matchingRecipe && matchingRecipe.name
+      };
+    }).sort((a, b) => (
+      (a && a.countryName ? a.countryName : '') > (b && b.countryName ? b.countryName : '')
+        ? -1 : 1
+    ));
+  }
 );
