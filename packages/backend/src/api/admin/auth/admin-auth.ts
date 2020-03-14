9651bfe856aaa2ac6c0982ca578bb2693846c19a
chrisberry4545 <chrisberry4545@googlemail.com>
2020-03-14 17:40:32 +0000

    import enums directly on the backend

--- packages/backend/src/api/admin/auth/admin-auth.ts
+++ packages/backend/src/api/admin/auth/admin-auth.ts
@@ -1,4 +1,4 @@
 import { appPassport } from './app-passport';
-import { USER_ROLES } from '@chrisb-dev/seasonal-shared';
+import { USER_ROLES } from '@chrisb-dev/seasonal-shared/dist/enums/user-roles.enum';
 export const adminAuth =
   appPassport.authenticate(USER_ROLES.ADMIN, { session: false });
