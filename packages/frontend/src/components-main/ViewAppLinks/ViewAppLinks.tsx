784fac4a9e85c66f005afa40d1eaf21a4d1cc7c2
Chris Berry <chrisberry4545@googlemail.com>
2020-03-14 17:42:47 +0000

    ios store link on web frontend (#31)

--- packages/frontend/src/components-main/ViewAppLinks/ViewAppLinks.tsx
+++ packages/frontend/src/components-main/ViewAppLinks/ViewAppLinks.tsx
@@ -1,17 +1,16 @@
 import React, { FC } from 'react';
-
 import './ViewAppLinks.scss';
-
 import {
   AndroidAppDownload
 } from '../AndroidAppDownload/AndroidAppDownload';
-
 import {
   ViewWebAppVersionConnector
 } from '../ViewWebAppVersion/ViewWebAppVersion.connector';
+import { IOSAppDownload } from '../IOSAppDownload/IOSAppDownload';
 
 export const ViewAppLinks: FC<{}> = () => (
   <div className='c-view-app-links'>
+    <IOSAppDownload />
     <AndroidAppDownload />
     <ViewWebAppVersionConnector />
   </div>
