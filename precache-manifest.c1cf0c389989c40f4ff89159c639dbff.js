94788d2bde59a0472eac83792567049c46165258
chrisbery4545 <chrisbery4545@googlemail.com>
2020-03-16 08:11:23 +0000

    Updates

---
+++ precache-manifest.c1cf0c389989c40f4ff89159c639dbff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a6945f8d86f8adc32faa0549a10bd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96188e1bbc42187b2e7b",
+    "url": "/static/css/main.c41ddbc8.chunk.css"
+  },
+  {
+    "revision": "208d0528d675ecff0dff",
+    "url": "/static/js/2.a8ed3b02.chunk.js"
+  },
+  {
+    "revision": "46ecbfe0fdbde78331aa24943b0f4868",
+    "url": "/static/js/2.a8ed3b02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96188e1bbc42187b2e7b",
+    "url": "/static/js/main.a3721a4d.chunk.js"
+  },
+  {
+    "revision": "342364179cda9dbf6144",
+    "url": "/static/js/runtime-main.d31a0f90.js"
+  }
+]);
