7b8329c3f1ff78deaffe12469eebce25b7d79d77
chrisberry4545 <chrisberry4545@googlemail.com>
2020-03-23 08:36:55 +0000

    do not hydrate after react loads, just rerender

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import { render } from 'react-dom';
 import './index.scss';
 import { App } from './components-app/App';
 import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById('root');
-if (rootElement && rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+render(<App />, rootElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
