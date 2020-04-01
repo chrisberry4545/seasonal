e9ac5a9c6a75ac18d9f3b7efa879e2ade7d9f37e
chrisberry4545 <chrisberry4545@googlemail.com>
2020-04-01 08:13:30 +0100

    stop splash image jumping

--- packages/frontend/src/components-main/FoodTable/FoodTable.tsx
+++ packages/frontend/src/components-main/FoodTable/FoodTable.tsx
@@ -22,7 +22,7 @@ export const FoodTable: FC<IFoodTableProps> = ({
       ? <div className='c-food-table__loading-spinner-wrapper'>
         <LoadingSpinner />
       </div>
-      : <Switable <ImageGrid data-e2e='food-grid'
+      : <ImageGrid data-e2e='food-grid'
           data={food} onClick={onFoodClick} />
     : null
 );
