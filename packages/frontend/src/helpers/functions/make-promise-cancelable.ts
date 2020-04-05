5983f2e26a54f588b3d0df35951758745b39d0b0
chrisberry4545 <chrisberry4545@googlemail.com>
2020-04-05 16:56:11 +0100

    removed animatiomn from images

--- packages/frontend/src/helpers/functions/make-promise-cancelable.ts
+++ packages/frontend/src/helpers/functions/make-promise-cancelable.ts
@@ -6,16 +6,16 @@ export const makePromiseCancelable = <T>(
   let hasCancelled = false;
 
   const wrappedPromise = new Promise<T>((resolve, reject) => {
-      promise.then(
-          (val) => hasCancelled ? reject({isCanceled: true}) : resolve(val),
-          (error) => hasCancelled ? reject({isCanceled: true}) : reject(error)
-      );
+    promise.then(
+      (val) => hasCancelled ? reject({ isCanceled: true }) : resolve(val),
+      (error) => hasCancelled ? reject({ isCanceled: true }) : reject(error)
+    );
   });
 
   return {
-      promise: wrappedPromise,
-      cancel() {
-          hasCancelled = true;
-      }
+    promise: wrappedPromise,
+    cancel() {
+      hasCancelled = true;
+    }
   };
 };
