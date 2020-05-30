187ede7234358c5e5335c3733e9e24744ea042ca
Chris Berry <chrisberry4545@googlemail.com>
2020-05-30 16:08:32 +0100

    settings page scroll fixes (#95)

--- packages/mobile/src/components-pages/SettingsPage/SettingsPage.tsx
+++ packages/mobile/src/components-pages/SettingsPage/SettingsPage.tsx
@@ -8,20 +8,24 @@ import {
 
 import { MainContainer, DefaultPaddingContainer } from '../../components-layout';
 import { View, ViewStyle } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const styleSettingsPageContent: ViewStyle = {
+  marginBottom: 48,
   marginTop: 16
 };
 
 export const SettingsPage: FC<{}> = () => (
   <MainContainer>
     <HeaderConnecter />
-    <DefaultPaddingContainer>
-      <View style={styleSettingsPageContent}>
-        <SelectLocationConnecter>
-          <SettingsBackButtonConnecter />
-        </SelectLocationConnecter>
-      </View>
-    </DefaultPaddingContainer>
+    <ScrollView>
+      <DefaultPaddingContainer>
+        <View style={styleSettingsPageContent}>
+          <SelectLocationConnecter>
+            <SettingsBackButtonConnecter />
+          </SelectLocationConnecter>
+        </View>
+      </DefaultPaddingContainer>
+    </ScrollView>
   </MainContainer>
 );
