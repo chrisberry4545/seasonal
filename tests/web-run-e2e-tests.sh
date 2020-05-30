9096221b900af9cbc0e0120794cf1caaaec319a4
chrisberry4545 <chrisberry4545@googlemail.com>
2020-05-30 14:18:08 +0100

    logging when outputing logs

--- tests/web-run-e2e-tests.sh
+++ tests/web-run-e2e-tests.sh
@@ -11,8 +11,11 @@ echo "Starting tests..."
 eval "docker-compose ${DOCKER_COMPOSE_FILES} run --service-ports --rm seasonal-e2e-web"
 status=$?
 
+echo "Writing logs..."
 mkdir -p logs
 eval "docker-compose ${DOCKER_COMPOSE_FILES} logs --no-color > ./logs/compose.log"
+cat  ./logs/compose.logs
+echo "Logs stored"
 
 eval "docker-compose ${DOCKER_COMPOSE_FILES} down"
 
