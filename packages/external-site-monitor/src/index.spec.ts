4a47a8274ed1034e585abddb44b96f36eb3450eb
chrisberry4545 <chrisberry4545@googlemail.com>
2020-07-03 08:37:54 +0100

    used status for external monitoring of recipe urls

--- packages/external-site-monitor/src/index.spec.ts
+++ packages/external-site-monitor/src/index.spec.ts
@@ -1,32 +1,20 @@
 import fetch from 'node-fetch';
 import recipes from './recipe-list/recipes.json';
-import cheerio from 'cheerio';
 
 describe('check recipes sites have not changed', () => {
 
-  const checkIfSiteBlocksCrawlers = (text: string): boolean =>
-    text.includes('Javascript is disabled or blocked by an extension');
-
-  const getContents = async (
-    url: string,
-    recipeName: string
-  ): Promise<string> => {
+  const getStatus = async (
+    url: string
+  ): Promise<number> => {
     const site = await fetch(url);
-    const text = await site.text();
-    if (checkIfSiteBlocksCrawlers(text)) {
-      return 'Site blocks crawlers - assuming nothing has changed';
-    }
-    const $ = cheerio.load(text);
-    const bodyText = $('body').text().toLowerCase();
-    const indexOfTitle = bodyText.indexOf(recipeName.slice(0, 10).toLowerCase());
-    return bodyText.slice(indexOfTitle, indexOfTitle + 500).replace(/\s/g, '');
+    return site.status;
   };
 
   beforeAll(() => jest.setTimeout(50000));
 
-  test.each(recipes)('%p recipe matches snapshot', async (recipe) => {
-    const allContent = await getContents(recipe.linkUrl, recipe.name);
-    expect(allContent).toMatchSnapshot();
+  test.each(recipes)('%p recipe appears to still exist', async (recipe) => {
+    const status = await getStatus(recipe.linkUrl);
+    expect(status).toMatchSnapshot(200);
   });
 
 });
--- packages/external-site-monitor/src/postgres/get-recipes.ts
+++ packages/external-site-monitor/src/postgres/get-recipes.ts
@@ -1,18 +1,16 @@
 import { getPostgresInstance } from './get-postgres-instance';
 
 export const getRecipes = async <T>(): Promise<Array<{
-  linkUrl: string,
-  name: string
+  linkUrl: string
 }>> => {
   const db = await getPostgresInstance();
-  const result = await db.query<{ name: string, link_url: string }>({
+  const result = await db.query<{ link_url: string }>({
     text: `
-      SELECT name, link_url FROM recipes
+      SELECT link_url FROM recipes
     `
   });
   db.release();
   return result.rows && result.rows.map((row) => ({
-    linkUrl: row.link_url,
-    name: row.name
+    linkUrl: row.link_url
   }));
 };
