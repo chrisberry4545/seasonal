64796c9c123f8409f9f81d78a2272c0aa20d98d1
chrisberry4545 <chrisberry4545@googlemail.com>
2020-07-29 08:04:07 +0100

    external site crawl update

--- packages/external-site-monitor/src/food-list/update-food.ts
+++ packages/external-site-monitor/src/food-list/update-food.ts
@@ -5,7 +5,7 @@ import { getFood } from '../postgres/get-food';
 export const updateFood = async (): Promise<boolean> => {
   try {
     const food = await getFood();
-    fs.writeFileSync(`${__dirname}/food.json`, JSON.stringify(food));
+    fs.writeFileSync(`${__dirname}/food.json`, JSON.stringify(food, null, 2));
     return true;
   } catch (e) {
     errorLogger.log(e, 'Failed to update food');
--- packages/external-site-monitor/src/recipe-list/update-recipes.ts
+++ packages/external-site-monitor/src/recipe-list/update-recipes.ts
@@ -8,7 +8,7 @@ import { errorLogger } from '../logger/logger';
 export const updateRecipes = async (): Promise<boolean> => {
   try {
     const recipeUrls = await getRecipes();
-    fs.writeFileSync(`${__dirname}/recipes.json`, JSON.stringify(recipeUrls));
+    fs.writeFileSync(`${__dirname}/recipes.json`, JSON.stringify(recipeUrls, null, 2));
     return true;
   } catch (e) {
     errorLogger.log(e, 'Failed to update recipes');
